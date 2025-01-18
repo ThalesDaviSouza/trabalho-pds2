b5b31e71f026e9d3c258ea17134785d4bd285c63
julia.bento <juliabento.ufmg@gmail.com>
2025-01-18 01:46:27 -0300

    Implementando o Lig4

--- include/tabuleiro.hpp
+++ include/tabuleiro.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "./../Enums/Cor.cpp"
+#include "./../Enums/Direcao.hpp"
 
 using namespace std;
 
@@ -17,14 +18,15 @@ public:
     void printTabuleiro();
     virtual bool verificarJogada(int x, int y, Cor cor);
     virtual void fazerJogada(int x, int y, Cor cor);
+    virtual bool verificarVitoria()=0;
 };
 
 class Tabuleiro_Lig4 : public Tabuleiro{ 
     public:
      Tabuleiro_Lig4();
      bool verificarJogada(int x, int y, Cor cor) override;
-     void fazerJogada (int y)override;
-     bool verificarVitoria(int tabuleiro_[6][7]);
+     void fazerJogada (int x, int y, Cor cor)override;
+     bool verificarVitoria(int x, int y, Cor cor_atual);
 }; 
 
 class Tabuleiro_JogoDaVelha : public Tabuleiro{ 
